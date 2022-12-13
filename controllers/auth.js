ed66bd5e2ecca085176ad05d1ce289495b43fabd
duvan.botero <duvis07>
2022-12-13 12:39:47 -0500

    Autenticacion con google y logout implementado

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const bcryptjs = require("bcryptjs");
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body; //Obtener el correo y el password del body
@@ -48,10 +49,47 @@ const login = async (req, res = response) => {
 const googleSignIn = async (req, res = response) => {
   const { id_token } = req.body;
 
-  res.json({
-    msg: "googleSignIn",
-    id_token,
-  });
+  try {
+    const { correo, nombre, img } = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      //Tengo que crear el usuario en la BD
+      const data = {
+        nombre,
+        correo,
+        password: ":P",
+        rol : "USER_ROLE",
+        img,
+        google: true,
+      };
+
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    //Si el usuario en BD está desactivado en la BD (estado: false) no se puede autenticar
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: "Hable con el administrador, usuario bloqueado",
+      });
+    }
+
+    //Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: "Token de Google no es válido",
+    });
+    console.log(error);
+  }
 };
 
 module.exports = {
